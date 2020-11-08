2c72be9269f4afd2a78feaaea61d51f26b35add1
hlii0085 <hlii0085@student.monash.edu>
2020-11-08 23:25:19 +1100

    FINAL VERSION 1.1

--- FIT5032_Assignment/FIT5032_Assignment/Controllers/BookEventsController.cs
+++ FIT5032_Assignment/FIT5032_Assignment/Controllers/BookEventsController.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Net;
 
 namespace FIT5032_Assignment.Controllers
 {
@@ -24,11 +26,30 @@ namespace FIT5032_Assignment.Controllers
         public ActionResult Book(int id)
         {
             var userId = User.Identity.GetUserId();
+            // check if the user already booked this event
             var check = db.BookEvents.Where(be => be.BookingId == id && be.ApplicationUserId.Equals(userId)).FirstOrDefault();
             var booking = db.Bookings.Where(be => be.Id == id).FirstOrDefault();
 
-            if(check == null)
+            // Conflict
+            var conflicts = db.BookEvents.Where(be => be.ApplicationUserId == userId).Include(be => be.Booking).ToList();
+            Boolean cf = false;
+            foreach(BookEvent conflict in conflicts)
             {
+                if(conflict.Booking.Time.Equals(booking.Time))
+                {
+                    cf = true;
+                    break;
+
+                }
+                
+            }
+
+            if (check == null)
+            {
+                if (cf)
+                {
+                    return View("Conflict");
+                }
                 var bookEvent = new BookEvent { ApplicationUserId = User.Identity.GetUserId(), BookingId = id };
                 db.BookEvents.Add(bookEvent);
                 db.SaveChanges();
@@ -39,11 +60,13 @@ namespace FIT5032_Assignment.Controllers
                 string content = "You have successfully booked an enquiry. The time is " + booking.Time.ToString();
                 EmailSender es = new EmailSender();
                 es.Send(toMail, subject, content);
-            } else
+                return View("Index");
+            }
+            else
             {
                 ViewBag.Error = "You have already booked this hospital!";
-            }            
-            return View("Index");
+                return View("Booked");
+            }
         }
 
         public JsonResult Bar()
